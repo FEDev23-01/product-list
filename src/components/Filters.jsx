3814e8798facad81a68e12d783a323c3bdccbad1
Ali Abdollahi <aliabdollahi3000@gmail.com>
2025-07-21 22:38:44 +0330

    Initial commit

---
+++ src/components/Filters.jsx
@@ -0,0 +1,19 @@
+export default function Filters({ onFilter }) {
+  return (
+    <aside className="w-1/4 p-4 bg-white pb-4">
+      <h2 className="text-lg font-semibold mb-4">Filters</h2>
+      <button
+        className="block w-full px-4 my-2 py-2 text-white rounded bg-blue-500"
+        onClick={() => onFilter("under500")}
+      >
+        Under $500
+      </button>
+      <button
+        className="block w-full px-4 my-2 py-2 text-white rounded bg-gray-500"
+        onClick={() => onFilter("all")}
+      >
+        Show all products
+      </button>
+    </aside>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 font-bold text-lg">
+      <h1>Woodmarket</h1>
+    </header>
+  );
+}
+export default Header;
